cf7f835c2c0964e28ccb9cd3799093789fb33161
Seoin Chai <sc5316@ic.ac.uk>
2018-01-27 19:45:59 +0000

    User voice detection successful

--- SilenceOfTheLabs/Assets/Voice Recognition/VoiceRecord.cs
+++ SilenceOfTheLabs/Assets/Voice Recognition/VoiceRecord.cs
@@ -6,11 +6,26 @@ public class VoiceRecord : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+//*
+        AudioSource aud = GetComponent<AudioSource>();
+        aud.clip = Microphone.Start("Built-in Microphone", true, 10, 44100);
+        aud.loop = true;
+        while(!(Microphone.GetPosition(null) > 0)) { }
+        aud.Play();
+/*/
+
+        var audio = GetComponent<AudioSource>();
+        audio.clip = Microphone.Start("Built-in Microphone", true, 10, 44100);
+        audio.loop = true;
+        while (!(Microphone.GetPosition(null) > 0)) { }
+            audio.Play();
+//*/
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+
 }
