db64a17450c1b60943d1071916129a0194257520
Seoin Chai <sc5316@ic.ac.uk>
2018-01-27 15:56:56 +0000

    Implemented speech  recognition feature

---
+++ SilenceOfTheLabs/Assets/Voice Recognition/VoiceRecognition.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using System.Linq;
+
+public class VoiceRecognition : MonoBehaviour {
+
+    KeywordRecognizer keywordRecognizer;
+    Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+
+	// Use this for initialization
+	void Start () {
+        keywords.Add("hello", () =>
+        {
+            onHelloCall();
+        });
+
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizerOnPhraseRecognized;
+        keywordRecognizer.Start();
+  
+	}
+
+    void KeywordRecognizerOnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        System.Action keywordAction;
+
+        if(keywords.TryGetValue(args.text, out keywordAction))
+        {
+            keywordAction.Invoke();
+        }
+    }
+
+    void onHelloCall()
+    {
+        print("Hello was called");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
