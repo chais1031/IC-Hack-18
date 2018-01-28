532f2689a24a6055f0f80bc2169b6858bca4bf6b
Seoin Chai <sc5316@ic.ac.uk>
2018-01-28 03:51:32 +0000

    Added a voicebar cube, voice and sound recognition algorithms

---
+++ SilenceOfTheLabs/Assets/Voice Recognition/AudioPermission.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPermission : MonoBehaviour {
+
+    // Use this for initialization
+    IEnumerator Start () {
+        yield return Application.RequestUserAuthorization(UserAuthorization.WebCam | UserAuthorization.Microphone);
+        if (Application.HasUserAuthorization(UserAuthorization.WebCam | UserAuthorization.Microphone)) {
+
+        }
+        else {
+
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ SilenceOfTheLabs/Assets/Voice Recognition/SoundBar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundBar : MonoBehaviour {
+
+    GameObject bar;
+
+	// Use this for initialization
+	void Start () {
+        bar = GameObject.Find("Cube");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        bar.transform.localScale = new Vector3(1, VoiceRecord.loudness/10, 1);
+	}
+}
